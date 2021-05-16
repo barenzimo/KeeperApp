a8279692f5d0577b12dbb53496247a455e7d8e20
Aman Tiwari <68875969+barenzimo@users.noreply.github.com>
2021-05-16 09:00:51 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Footer />
+      <Header />
+      <Note />
+    </div>
+  );
+}
+export default App;
